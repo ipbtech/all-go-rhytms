83f99099972928ff0b64490e94b2d93e1f5c7ad7
Петров Илья Вадимович <ipb.tech@yandex.ru>
2025-01-21 13:18:49 +0300

    linear searching

---
+++ AllGoRhytms/Searching/LinearSearching.cs
@@ -0,0 +1,59 @@
+﻿namespace AllGoRhytms.Searching
+{
+    public class LinearSearching
+    {
+        [Theory]
+        [InlineData(21, 5, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(18, 2, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(50, 10, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(5, 0, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(80, 13, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(1, -1, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        public void LinearSearchFirstIndex(int searchValue, int expectedInd, params int[] inputArray)
+        {
+            // Arrange
+            int actualInd = -1;
+
+            // Arrange
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == searchValue)
+                {
+                    actualInd = i;
+                    break;
+                }
+            }
+
+            //Assert
+            Assert.Equal(expectedInd, actualInd);
+        }
+
+
+        [Theory]
+        [InlineData(21, 8, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(18, 3, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(50, 11, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(5, 0, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(80, 13, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(1, -1, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        [InlineData(5, 4, new int[] { 5, 5, 5, 5, 5, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+        public void LinearSearchLastIndex(int searchValue, int expectedInd, params int[] inputArray)
+        {
+            // Arrange
+            int actualInd = -1;
+
+            // Arrange
+            for (int i = inputArray.Length - 1; i >= 0; i--)
+            {
+                if (inputArray[i] == searchValue)
+                {
+                    actualInd = i;
+                    break;
+                }
+            }
+
+            //Assert
+            Assert.Equal(expectedInd, actualInd);
+        }
+    }
+}
--- Benchmarks/Benchmarks/SearchingAlgorithms.cs
+++ Benchmarks/Benchmarks/SearchingAlgorithms.cs
@@ -8,20 +8,19 @@ namespace Benchmarks
     {
         private JumpSearching _jumpSearching = new JumpSearching();
         private BinarySearching _binarySearching = new BinarySearching();
+        private LinearSearching _linearSearching = new LinearSearching();
 
         private int[] _inputArray = { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 };
         ///[InlineData(21, 5, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+
+
+        [Benchmark]
+        public void LinearSearchFirstIndex() => _linearSearching.LinearSearchFirstIndex(21, 5, _inputArray);
 
         [Benchmark]
         public void JumpSearchFirstIndex() => _jumpSearching.JumpSearchFirstIndex(21, 5, _inputArray);
 
         [Benchmark]
-        public void JumpSearchLastIndex() => _jumpSearching.JumpSearchLastIndex(21, 5, _inputArray);
-
-        [Benchmark]
         public void BinarySearchFirstIndex() => _binarySearching.BinarySearchFirstIndex(21, 5, _inputArray);
-
-        [Benchmark]
-        public void BinarySearchLastIndex() => _binarySearching.BinarySearchLastIndex(21, 5, _inputArray);
     }
 }
