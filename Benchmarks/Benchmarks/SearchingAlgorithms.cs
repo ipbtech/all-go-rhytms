ff2a62ecd7d792c006b2f39475c6f8ebcb5fcad7
Петров Илья Вадимович <ipb.tech@yandex.ru>
2025-01-21 13:05:22 +0300

    add benchmarks for searching

---
+++ Benchmarks/Benchmarks/SearchingAlgorithms.cs
@@ -0,0 +1,27 @@
+﻿using AllGoRhytms.Searching;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SearchingAlgorithms
+    {
+        private JumpSearching _jumpSearching = new JumpSearching();
+        private BinarySearching _binarySearching = new BinarySearching();
+
+        private int[] _inputArray = { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 };
+        ///[InlineData(21, 5, new int[] { 5, 16, 18, 18, 20, 21, 21, 21, 21, 45, 50, 50, 75, 80 })]
+
+        [Benchmark]
+        public void JumpSearchFirstIndex() => _jumpSearching.JumpSearchFirstIndex(21, 5, _inputArray);
+
+        [Benchmark]
+        public void JumpSearchLastIndex() => _jumpSearching.JumpSearchLastIndex(21, 5, _inputArray);
+
+        [Benchmark]
+        public void BinarySearchFirstIndex() => _binarySearching.BinarySearchFirstIndex(21, 5, _inputArray);
+
+        [Benchmark]
+        public void BinarySearchLastIndex() => _binarySearching.BinarySearchLastIndex(21, 5, _inputArray);
+    }
+}
